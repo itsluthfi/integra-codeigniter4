dd75875e8b977579ba57b330078ac085903e0011
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-01-25 10:35:45 +0700

    feat: update dashboard content

--- app/Controllers/Admin/DashboardController.php
+++ app/Controllers/Admin/DashboardController.php
@@ -9,7 +9,12 @@ class DashboardController extends BaseController
     public function index()
     {
         $data = [
-            'title' => 'Dashboard'
+            'title' => 'Dashboard',
+            'product_itconsultant' => $this->ProductModel->where('category_slug', 'it-consultant')->countAllResults(),
+            'product_webappdev' => $this->ProductModel->where('category_slug', 'web-app-development')->countAllResults(),
+            'product_mobileappdev' => $this->ProductModel->where('category_slug', 'mobile-app-development')->countAllResults(),
+            'product_training' => $this->ProductModel->where('category_slug', 'training')->countAllResults(),
+            'product' => $this->ProductModel->limit(3)->find(),
         ];
 
         return view('admin/dashboard/index', $data);
