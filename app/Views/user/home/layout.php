be52e96cad51a76dd5c3347094c20e696c606fc2
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-01-26 13:43:56 +0700

    feat: update header position on each front-end pages

--- app/Views/user/home/layout.php
+++ app/Views/user/home/layout.php
@@ -37,48 +37,6 @@
   ======================================================== -->
 </head>
 
-<!-- ======= Header ======= -->
-<header id="header" class="d-flex align-items-center">
-    <div class="container d-flex align-items-center justify-content-between">
-
-        <div class="logo">
-            <!-- <h1 class="text-light"><a href="<?= base_url('/') ?>"><span>Integra</span></a></h1> -->
-            <!-- Uncomment above if you prefer to use an text logo -->
-            <a href="<?= base_url('/') ?>"><img src="<?= base_url('assets') ?>/img/logo-integra.png" alt="" class="img-fluid"></a>
-        </div>
-
-        <nav id="navbar" class="navbar">
-            <ul>
-                <li><a class="nav-link scrollto active" href="<?= base_url('home') ?>">Home</a></li>
-                <li><a class="nav-link scrollto" href="#about">Tentang Kami</a></li>
-                <li><a class="nav-link scrollto" href="#services">Layanan</a></li>
-                <li><a class="nav-link scrollto" href="#portfolio">Portfolio</a></li>
-                <li><a class="nav-link scrollto" href="#team">Tim</a></li>
-                <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-                    <ul>
-                        <li><a href="#">Drop Down 1</a></li>
-                        <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                            <ul>
-                                <li><a href="#">Deep Drop Down 1</a></li>
-                                <li><a href="#">Deep Drop Down 2</a></li>
-                                <li><a href="#">Deep Drop Down 3</a></li>
-                                <li><a href="#">Deep Drop Down 4</a></li>
-                                <li><a href="#">Deep Drop Down 5</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">Drop Down 2</a></li>
-                        <li><a href="#">Drop Down 3</a></li>
-                        <li><a href="#">Drop Down 4</a></li>
-                    </ul>
-                </li>
-                <li><a class="nav-link scrollto" href="#contact">Kontak</a></li>
-            </ul>
-            <i class="bi bi-list mobile-nav-toggle"></i>
-        </nav><!-- .navbar -->
-
-    </div>
-</header><!-- End Header -->
-
 <?= $this->renderSection('content') ?>
 
 <!-- ======= Footer ======= -->
